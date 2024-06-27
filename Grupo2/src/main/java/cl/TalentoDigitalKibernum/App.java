a641234c9b6a34b26752f2324d3fcaa2fbf7ca14
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2024-06-26 22:55:53 -0400

    Subir proyecto java maven Arquetipo

---
+++ Grupo2/src/main/java/cl/TalentoDigitalKibernum/App.java
@@ -0,0 +1,13 @@
+package cl.TalentoDigitalKibernum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven Grupo2!" );
+    }
+}
